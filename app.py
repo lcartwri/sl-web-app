20ba3b94df93e6e72b7fd5ca804e3dd3dcabf66b
lcartwri <lcartwri@cisco.com>
2020-11-26 17:15:11 +0000

    html issue still persist but deleted original forms.py file

--- app.py
+++ app.py
@@ -4,9 +4,20 @@ from flask import render_template
 
 
 
+#####@app.route('/')
+#####def home():
+#####    return render_template('home.html')
+
 @app.route('/')
-def home():
-    return render_template('home.html')
+@app.route('/<page>')
+def home(page=None):
+   if page:
+      # called with page parameter
+      return render_template('%s.html' % page)
+   else:
+      # called with no parameters 
+      return render_template('home.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
